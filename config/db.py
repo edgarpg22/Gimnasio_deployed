018adb6642778dc66c79918767738283e0325170
Edgar <210519@utxicotepec.edu.mx>
2024-07-19 16:53:26 -0600

    modificaion en db

--- config/db.py
+++ config/db.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_TUUjXCnZQAQk-kZ2VKZ@mysql-270c42e9-lorenaascencion2003-2691.d.aivencloud.com:10171/defaultdb"
+
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_wsRaH9f-pWSVw4kvRRy@mysql-19ac5bf0-utxicotepec-b6a5.l.aivencloud.com:18583/defaultdb"
 
 #  Conexión local
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
